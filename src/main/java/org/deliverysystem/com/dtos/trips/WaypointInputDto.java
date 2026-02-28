dc68fb777f38dc1183f144e656daddfef38cad2f
Yaroslav Guz <aroslavguz@gmail.com>
2026-02-28 12:07:44 +0200

    Update dtos for trip creation

--- src/main/java/org/deliverysystem/com/dtos/trips/WaypointInputDto.java
+++ src/main/java/org/deliverysystem/com/dtos/trips/WaypointInputDto.java
@@ -1,15 +1,17 @@
 package org.deliverysystem.com.dtos.trips;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Проміжне місто маршруту рейсу")
 public record WaypointInputDto(
-        @NotNull
+        @NotNull(message = "Місто маршруту не може бути порожнім")
         @Schema(description = "ID міста", example = "145")
         Integer cityId,
 
-        @NotNull
+        @NotNull(message = "Порядковий номер є обов'язковим")
+        @Min(value = 0, message = "Порядковий номер не може бути від'ємним")
         @Schema(description = "Порядковий номер у маршруті", example = "1")
         Integer sequenceNumber
 ) {}
