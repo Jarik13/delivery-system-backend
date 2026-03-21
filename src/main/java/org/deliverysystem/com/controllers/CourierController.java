2c6bae82a0bc88b63a91c2d3f70e4ff735983774
Yaroslav Guz <aroslavguz@gmail.com>
2026-03-21 17:38:05 +0200

    Add to courier dto hasActiveRouteList attribute

--- src/main/java/org/deliverysystem/com/controllers/CourierController.java
+++ src/main/java/org/deliverysystem/com/controllers/CourierController.java
@@ -1,16 +1,47 @@
 package org.deliverysystem.com.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.deliverysystem.com.dtos.CourierDto;
 import org.deliverysystem.com.services.impl.CourierService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/couriers")
 @Tag(name = "Couriers", description = "Кур'єри адресної доставки")
-public class CourierController extends AbstractBaseController<CourierDto, Integer> {
-    public CourierController(CourierService service) {
-        super(service);
+@RequiredArgsConstructor
+public class CourierController {
+    private final CourierService courierService;
+
+    @GetMapping
+    @Operation(summary = "Отримати всіх кур'єрів")
+    public ResponseEntity<List<CourierDto>> getAll() {
+        return ResponseEntity.ok(courierService.findAllWithActiveRouteListStatus());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourierDto> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(courierService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourierDto> create(@Valid @RequestBody CourierDto dto) {
+        return ResponseEntity.ok(courierService.create(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CourierDto> update(@PathVariable Integer id, @Valid @RequestBody CourierDto dto) {
+        return ResponseEntity.ok(courierService.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        courierService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
