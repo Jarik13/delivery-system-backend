73436319e851d8f30802b69a01d78dcbdf7a7df1
Yaroslav Guz <aroslavguz@gmail.com>
2026-03-28 13:56:01 +0200

    Fix N+1 problem with ShipmentWaybillRepository

--- src/main/java/org/deliverysystem/com/repositories/ShipmentWaybillRepository.java
+++ src/main/java/org/deliverysystem/com/repositories/ShipmentWaybillRepository.java
@@ -3,6 +3,7 @@ package org.deliverysystem.com.repositories;
 import org.deliverysystem.com.entities.ShipmentWaybill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
@@ -11,8 +12,26 @@ import java.util.Optional;
 
 @Repository
 public interface ShipmentWaybillRepository extends JpaRepository<ShipmentWaybill, Integer> {
-    @Query("SELECT MAX(sw.sequenceNumber) FROM ShipmentWaybill sw WHERE sw.waybill.id = ?1")
-    Optional<Integer> findMaxSequenceNumberByWaybillId(Integer id);
+    @Query("""
+                SELECT sw FROM ShipmentWaybill sw
+                LEFT JOIN FETCH sw.shipment s
+                LEFT JOIN FETCH s.originDeliveryPoint odp
+                LEFT JOIN FETCH odp.deliveryPoint odpp
+                LEFT JOIN FETCH odpp.city
+                LEFT JOIN FETCH s.destinationDeliveryPoint ddp
+                LEFT JOIN FETCH ddp.deliveryPoint ddpp
+                LEFT JOIN FETCH ddpp.city
+                LEFT JOIN FETCH s.payments
+                LEFT JOIN FETCH s.parcel
+                LEFT JOIN FETCH s.shipmentStatus
+                LEFT JOIN FETCH s.shipmentType
+                LEFT JOIN FETCH s.sender
+                LEFT JOIN FETCH s.recipient
+                WHERE sw.waybill.id = :waybillId
+                ORDER BY sw.sequenceNumber
+            """)
+    List<ShipmentWaybill> findByWaybillIdOrderBySequenceNumber(@Param("waybillId") Integer waybillId);
 
-    List<ShipmentWaybill> findByWaybillIdOrderBySequenceNumber(Integer waybillId);
+    @Query("SELECT COALESCE(MAX(sw.sequenceNumber), 0) FROM ShipmentWaybill sw WHERE sw.waybill.id = :waybillId")
+    Optional<Integer> findMaxSequenceNumberByWaybillId(@Param("waybillId") Integer waybillId);
 }
